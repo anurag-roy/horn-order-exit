9899de874ae48752c57d1111ff49e4f9e5b6e90c
Anurag Roy <anurag@2pirad.com>
2020-10-29 02:01:22 +0530

    Added 0.25 to buyersBids

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const apiKey = process.env.API_KEY;
 const accessToken = process.env.ACCESS_TOKEN;
 
 const kc = new KiteConnect({
-  api_key: apiKey,
+	api_key: apiKey,
 });
 kc.setAccessToken(accessToken);
 
@@ -23,148 +23,148 @@ app.use(express.static(path.join(__dirname, "build")));
 app.use("/mapper", mapperRouter);
 
 app.post("/orderHornExit", ({ body }, response) => {
-  orderHornExit(body.stockA, body.stockB, body.stockC, body.stockD, Number(body.exitPrice));
-  response.send("Check console.");
+	orderHornExit(body.stockA, body.stockB, body.stockC, body.stockD, Number(body.exitPrice));
+	response.send("Check console.");
 });
 
 // Order function
 const order = async (stock, price) => {
-  const timestamp = new Date();
-  console.log(
-    `Order placed for ${stock.exchange}:${stock.tradingsymbol}, Transaction: ${stock.transactionType}, product: ${stock.product}, quantity: ${stock.quantity}, price: ${price}`,
-  );
-  console.log(`Time of order: ${timestamp.toUTCString()}`);
+	const timestamp = new Date();
+	console.log(
+		`Order placed for ${stock.exchange}:${stock.tradingsymbol}, Transaction: ${stock.transactionType}, product: ${stock.product}, quantity: ${stock.quantity}, price: ${price}`,
+	);
+	console.log(`Time of order: ${timestamp.toUTCString()}`);
 
-  return kc.placeOrder("regular", {
-    exchange: stock.exchange,
-    tradingsymbol: stock.tradingsymbol,
-    transaction_type: stock.transactionType,
-    quantity: stock.quantity,
-    product: stock.product,
-    price: price,
-    order_type: "LIMIT",
-  });
+	return kc.placeOrder("regular", {
+		exchange: stock.exchange,
+		tradingsymbol: stock.tradingsymbol,
+		transaction_type: stock.transactionType,
+		quantity: stock.quantity,
+		product: stock.product,
+		price: price,
+		order_type: "LIMIT",
+	});
 
-  // return `Order placed for ${stock.exchange}:${stock.tradingsymbol}, Transaction: ${stock.transactionType}, product: ${stock.product}, quantity: ${stock.quantity}`;
+	// return `Order placed for ${stock.exchange}:${stock.tradingsymbol}, Transaction: ${stock.transactionType}, product: ${stock.product}, quantity: ${stock.quantity}`;
 };
 
 const placeOrder = async (stockArray, priceArray) => {
-  const promiseArray = [];
+	const promiseArray = [];
 
-  for (let i = 0; i < 4; i++) {
-    promiseArray.push(order(stockArray[i], priceArray[i] + 0.1));
-  }
+	for (let i = 0; i < 4; i++) {
+		promiseArray.push(order(stockArray[i], priceArray[i] + 0.1));
+	}
 
-  await Promise.all(promiseArray);
+	await Promise.all(promiseArray);
 
-  const positions = await kc.getPositions();
-  console.log(positions);
+	const positions = await kc.getPositions();
+	console.log(positions);
 };
 
 const orderHornExit = (stockA, stockB, stockC, stockD, exitPrice) => {
-  // Extract instruments tokens for each stock
-  const aToken = parseInt(stockA.instrument_token);
-  const bToken = parseInt(stockB.instrument_token);
-  const cToken = parseInt(stockC.instrument_token);
-  const dToken = parseInt(stockD.instrument_token);
+	// Extract instruments tokens for each stock
+	const aToken = parseInt(stockA.instrument_token);
+	const bToken = parseInt(stockB.instrument_token);
+	const cToken = parseInt(stockC.instrument_token);
+	const dToken = parseInt(stockD.instrument_token);
 
-  // Extract quantity for each stock
-  const aQty = stockA.quantity;
-  const bQty = stockB.quantity;
-  const cQty = stockC.quantity;
-  const dQty = stockD.quantity;
+	// Extract quantity for each stock
+	const aQty = stockA.quantity;
+	const bQty = stockB.quantity;
+	const cQty = stockC.quantity;
+	const dQty = stockD.quantity;
 
-  // Extract position values for each stock
-  const aPv = stockA.positionValue;
-  const bPv = stockB.positionValue;
-  const cPv = stockC.positionValue;
-  const dPv = stockD.positionValue;
+	// Extract position values for each stock
+	const aPv = stockA.positionValue;
+	const bPv = stockB.positionValue;
+	const cPv = stockC.positionValue;
+	const dPv = stockD.positionValue;
 
-  // Declare variables which will be updated on each tick
-  let aSellersBid, bBuyersBid, cBuyersBid, dSellersBid;
+	// Declare variables which will be updated on each tick
+	let aSellersBid, bBuyersBid, cBuyersBid, dSellersBid;
 
-  // Flag to determine if order is already placed or not
-  let placedOrder = false;
+	// Flag to determine if order is already placed or not
+	let placedOrder = false;
 
-  // Exit Condition for HORN strategy
-  const lookForExit = () => {
-    const a = (0 + aPv - aSellersBid) * aQty;
-    const b = (0 - bPv + bBuyersBid) * bQty;
-    const c = (0 - cPv + cBuyersBid) * cQty;
-    const d = (0 + dPv - dSellersBid) * dQty;
+	// Exit Condition for HORN strategy
+	const lookForExit = () => {
+		const a = (0 + aPv - aSellersBid) * aQty;
+		const b = (0 - bPv + bBuyersBid) * bQty;
+		const c = (0 - cPv + cBuyersBid) * cQty;
+		const d = (0 + dPv - dSellersBid) * dQty;
 
-    const net = (a + b + c + d) / 75;
+		const net = (a + b + c + d) / 75;
 
-    if (net > exitPrice) {
-      console.log(`Net: ${net}, Exit Price: ${exitPrice}. Condition satisfied.`);
-      return true;
-    }
+		if (net > exitPrice) {
+			console.log(`Net: ${net}, Exit Price: ${exitPrice}. Condition satisfied.`);
+			return true;
+		}
 
-    console.log(`Net: ${net}, Exit Price: ${exitPrice}. Condition not satisfied.`);
-    return false;
-  };
+		console.log(`Net: ${net}, Exit Price: ${exitPrice}. Condition not satisfied.`);
+		return false;
+	};
 
-  const ticker = new KiteTicker({
-    api_key: apiKey,
-    access_token: accessToken,
-  });
+	const ticker = new KiteTicker({
+		api_key: apiKey,
+		access_token: accessToken,
+	});
 
-  ticker.connect();
+	ticker.connect();
 
-  ticker.on("connect", () => {
-    console.log("Subscribing to stocks...");
-    const items = [aToken, bToken, cToken, dToken];
-    ticker.subscribe(items);
-    ticker.setMode(ticker.modeFull, items);
-  });
+	ticker.on("connect", () => {
+		console.log("Subscribing to stocks...");
+		const items = [aToken, bToken, cToken, dToken];
+		ticker.subscribe(items);
+		ticker.setMode(ticker.modeFull, items);
+	});
 
-  ticker.on("ticks", (ticks) => {
-    if (!placedOrder) {
-      // Check tick and update corresponding stock bid price
-      // 1st Seller's Bud for stock to BUY
-      // 2nd Buyer's Bid for stock to SELL
-      ticks.forEach((t) => {
-        if (t.instrument_token == aToken) {
-          if (t.depth) {
-            if (t.depth.sell) {
-              aSellersBid = t.depth.sell[0].price;
-            }
-          }
-        } else if (t.instrument_token == bToken) {
-          if (t.depth) {
-            if (t.depth.buy) {
-              bBuyersBid = t.depth.buy[1].price;
-            }
-          }
-        } else if (t.instrument_token == cToken) {
-          if (t.depth) {
-            if (t.depth.buy) {
-              cBuyersBid = t.depth.buy[1].price;
-            }
-          }
-        } else if (t.instrument_token == dToken) {
-          if (t.depth) {
-            if (t.depth.sell) {
-              dSellersBid = t.depth.sell[0].price;
-            }
-          }
-        }
-      });
+	ticker.on("ticks", (ticks) => {
+		if (!placedOrder) {
+			// Check tick and update corresponding stock bid price
+			// 1st Seller's Bud for stock to BUY
+			// 2nd Buyer's Bid for stock to SELL
+			ticks.forEach((t) => {
+				if (t.instrument_token == aToken) {
+					if (t.depth) {
+						if (t.depth.sell) {
+							aSellersBid = t.depth.sell[0].price;
+						}
+					}
+				} else if (t.instrument_token == bToken) {
+					if (t.depth) {
+						if (t.depth.buy) {
+							bBuyersBid = t.depth.buy[1].price;
+						}
+					}
+				} else if (t.instrument_token == cToken) {
+					if (t.depth) {
+						if (t.depth.buy) {
+							cBuyersBid = t.depth.buy[1].price;
+						}
+					}
+				} else if (t.instrument_token == dToken) {
+					if (t.depth) {
+						if (t.depth.sell) {
+							dSellersBid = t.depth.sell[0].price;
+						}
+					}
+				}
+			});
 
-      // Look for Exit
-      if (lookForExit()) {
-        placedOrder = true;
-        placeOrder(
-          [stockA, stockB, stockC, stockD],
-          [aSellersBid, bBuyersBid, cBuyersBid, dSellersBid],
-        );
-      }
-    } else if (placedOrder) {
-      ticker.disconnect();
-    }
-  });
+			// Look for Exit
+			if (lookForExit()) {
+				placedOrder = true;
+				placeOrder(
+					[stockA, stockB, stockC, stockD],
+					[aSellersBid, bBuyersBid + 0.25, cBuyersBid + 0.25, dSellersBid],
+				);
+			}
+		} else if (placedOrder) {
+			ticker.disconnect();
+		}
+	});
 };
 
 app.listen(4004, () => {
-  console.log("Horn Exit Order started on http://localhost:4004");
+	console.log("Horn Exit Order started on http://localhost:4004");
 });
